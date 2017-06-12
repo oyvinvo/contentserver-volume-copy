ddf197fb0a13039b713e19567bb3d6db2f6b24b5
Øyvind Volden <oyvinvo@gmail.com>
2017-06-12 14:16:33 +0200

    Fixed the directory structure

--- copy-content-server-volumes.sh
+++ copy-content-server-volumes.sh
@@ -13,18 +13,18 @@ UUID=$(cat /proc/sys/kernel/random/uuid)
 ssh $1 << EOF
 	docker stop ${3}
 	mkdir $UUID
-	docker run --rm -v ~/$UUID:/backup --volumes-from ${3} debian:jessie tar -czvf /backup/thumbsrv.tar.gz /opt/dctm/product/7.3/thumbsrv/conf
-	docker run --rm -v ~/$UUID:/backup --volumes-from ${3} debian:jessie tar -czvf /backup/data.tar.gz /opt/dctm/data
-	docker run --rm -v ~/$UUID:/backup --volumes-from ${3} debian:jessie tar -czvf /backup/share.tar.gz /opt/dctm/share
-	docker run --rm -v ~/$UUID:/backup --volumes-from ${3} debian:jessie tar -czvf /backup/mdserverlogs.tar.gz /opt/dctm/wildfly9.0.1/server/DctmServer_MethodServer/logs
-	docker run --rm -v ~/$UUID:/backup --volumes-from ${3} debian:jessie tar -czvf /backup/obdc.tar.gz /opt/dctm/odbc
-	docker run --rm -v ~/$UUID:/backup --volumes-from ${3} debian:jessie tar -czvf /backup/xhiveconnector.tar.gz /opt/dctm/wildfly9.0.1/server/DctmServer_MethodServer/deployments/XhiveConnector.ear
-	docker run --rm -v ~/$UUID:/backup --volumes-from ${3} debian:jessie tar -czvf /backup/mdserverlog.tar.gz /opt/dctm/wildfly9.0.1/server/DctmServer_MethodServer/log
-	docker run --rm -v ~/$UUID:/backup --volumes-from ${3} debian:jessie tar -czvf /backup/dfc.tar.gz /opt/dctm/config
-	docker run --rm -v ~/$UUID:/backup --volumes-from ${3} debian:jessie tar -czvf /backup/mdserverconf.tar.gz /opt/dctm/mdserver_conf
-	docker run --rm -v ~/$UUID:/backup --volumes-from ${3} debian:jessie tar -czvf /backup/thumbwebinf.tar.gz /opt/dctm/product/7.3/thumbsrv/container/webapps/thumbsrv/WEB-INF
-	docker run --rm -v ~/$UUID:/backup --volumes-from ${3} debian:jessie tar -czvf /backup/dba.tar.gz /opt/dctm/dba
-	docker run --rm -v ~/$UUID:/backup --volumes-from ${3} debian:jessie tar -czvf /backup/xhivestorage.tar.gz /opt/dctm/xhive_storage
+	docker run --rm -v ~/$UUID:/backup --volumes-from ${3} debian:jessie tar -czvf /backup/thumbsrv.tar.gz -C /opt/dctm/product/7.3/thumbsrv/conf .
+	docker run --rm -v ~/$UUID:/backup --volumes-from ${3} debian:jessie tar -czvf /backup/data.tar.gz -C /opt/dctm/data .
+	docker run --rm -v ~/$UUID:/backup --volumes-from ${3} debian:jessie tar -czvf /backup/share.tar.gz -C /opt/dctm/share .
+	docker run --rm -v ~/$UUID:/backup --volumes-from ${3} debian:jessie tar -czvf /backup/mdserverlogs.tar.gz -C /opt/dctm/wildfly9.0.1/server/DctmServer_MethodServer/logs .
+	docker run --rm -v ~/$UUID:/backup --volumes-from ${3} debian:jessie tar -czvf /backup/obdc.tar.gz -C /opt/dctm/odbc .
+	docker run --rm -v ~/$UUID:/backup --volumes-from ${3} debian:jessie tar -czvf /backup/xhiveconnector.tar.gz -C /opt/dctm/wildfly9.0.1/server/DctmServer_MethodServer/deployments/XhiveConnector.ear .
+	docker run --rm -v ~/$UUID:/backup --volumes-from ${3} debian:jessie tar -czvf /backup/mdserverlog.tar.gz -C /opt/dctm/wildfly9.0.1/server/DctmServer_MethodServer/log .
+	docker run --rm -v ~/$UUID:/backup --volumes-from ${3} debian:jessie tar -czvf /backup/dfc.tar.gz -C /opt/dctm/config .
+	docker run --rm -v ~/$UUID:/backup --volumes-from ${3} debian:jessie tar -czvf /backup/mdserverconf.tar.gz -C /opt/dctm/mdserver_conf .
+	docker run --rm -v ~/$UUID:/backup --volumes-from ${3} debian:jessie tar -czvf /backup/thumbwebinf.tar.gz -C /opt/dctm/product/7.3/thumbsrv/container/webapps/thumbsrv/WEB-INF .
+	docker run --rm -v ~/$UUID:/backup --volumes-from ${3} debian:jessie tar -czvf /backup/dba.tar.gz -C /opt/dctm/dba .
+	docker run --rm -v ~/$UUID:/backup --volumes-from ${3} debian:jessie tar -czvf /backup/xhivestorage.tar.gz -C /opt/dctm/xhive_storage .
 EOF
 
 # copy backups from source to target server
@@ -50,18 +50,18 @@ EOF
 
 # restore the backups into the newly created volumes on the new server.
 ssh $2 << EOF
-	docker run --rm -v dctmcs_${4}_Thumbnail_Server_conf:/opt -v ~/$UUID:/backup debian:jessie tar zxvf /backup/thumbsrv.tar.gz
-	docker run --rm -v dctmcs_${4}_data:/opt -v ~/$UUID:/backup debian:jessie tar zxvf /backup/data.tar.gz
-	docker run --rm -v dctmcs_${4}_share:/opt -v ~/$UUID:/backup debian:jessie tar zxvf /backup/share.tar.gz
-	docker run --rm -v dctmcs_${4}_mdserver_logs:/opt -v ~/$UUID:/backup debian:jessie tar zxvf /backup/mdserverlogs.tar.gz
-	docker run --rm -v dctmcs_${4}_odbc:/opt -v ~/$UUID:/backup debian:jessie tar zxvf /backup/obdc.tar.gz
-	docker run --rm -v dctmcs_${4}_XhiveConnector:/opt -v ~/$UUID:/backup debian:jessie tar zxvf /backup/xhiveconnector.tar.gz
-	docker run --rm -v dctmcs_${4}_mdserver_log:/opt -v ~/$UUID:/backup debian:jessie tar zxvf /backup/mdserverlog.tar.gz
-	docker run --rm -v dctmcs_${4}_dfc:/opt -v ~/$UUID:/backup debian:jessie tar zxvf /backup/dfc.tar.gz
-	docker run --rm -v dctmcs_${4}_mdserver_conf:/opt -v ~/$UUID:/backup debian:jessie tar zxvf /backup/mdserverconf.tar.gz
-	docker run --rm -v dctmcs_${4}_Thumbnail_Server_webinf:/opt -v ~/$UUID:/backup debian:jessie tar zxvf /backup/thumbwebinf.tar.gz
-	docker run --rm -v dctmcs_${3}_dba:/opt -v ~/$UUID:/backup debian:jessie tar zxvf /backup/dba.tar.gz
-	docker run --rm -v dctmcs_${3}_xhive_storage:/opt -v ~/$UUID:/backup debian:jessie tar zxvf /backup/xhivestorage.tar.gz
+	docker run --rm -v dctmcs_${4}_Thumbnail_Server_conf:/opt -v ~/$UUID:/backup debian:jessie tar zxvf /backup/thumbsrv.tar.gz -C /opt
+	docker run --rm -v dctmcs_${4}_data:/opt -v ~/$UUID:/backup debian:jessie tar zxvf /backup/data.tar.gz -C /opt
+	docker run --rm -v dctmcs_${4}_share:/opt -v ~/$UUID:/backup debian:jessie tar zxvf /backup/share.tar.gz -C /opt
+	docker run --rm -v dctmcs_${4}_mdserver_logs:/opt -v ~/$UUID:/backup debian:jessie tar zxvf /backup/mdserverlogs.tar.gz -C /opt
+	docker run --rm -v dctmcs_${4}_odbc:/opt -v ~/$UUID:/backup debian:jessie tar zxvf /backup/obdc.tar.gz -C /opt
+	docker run --rm -v dctmcs_${4}_XhiveConnector:/opt -v ~/$UUID:/backup debian:jessie tar zxvf /backup/xhiveconnector.tar.gz -C /opt
+	docker run --rm -v dctmcs_${4}_mdserver_log:/opt -v ~/$UUID:/backup debian:jessie tar zxvf /backup/mdserverlog.tar.gz -C /opt
+	docker run --rm -v dctmcs_${4}_dfc:/opt -v ~/$UUID:/backup debian:jessie tar zxvf /backup/dfc.tar.gz -C /opt
+	docker run --rm -v dctmcs_${4}_mdserver_conf:/opt -v ~/$UUID:/backup debian:jessie tar zxvf /backup/mdserverconf.tar.gz -C /opt
+	docker run --rm -v dctmcs_${4}_Thumbnail_Server_webinf:/opt -v ~/$UUID:/backup debian:jessie tar zxvf /backup/thumbwebinf.tar.gz -C /opt
+	docker run --rm -v dctmcs_${3}_dba:/opt -v ~/$UUID:/backup debian:jessie tar zxvf /backup/dba.tar.gz -C /opt
+	docker run --rm -v dctmcs_${3}_xhive_storage:/opt -v ~/$UUID:/backup debian:jessie tar zxvf /backup/xhivestorage.tar.gz -C /opt
 EOF
 
 # clean up local, source and target backups.
